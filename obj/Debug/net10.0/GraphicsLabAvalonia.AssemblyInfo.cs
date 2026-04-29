48e62feadca24e6fcedab846a9219db4843c5fe1
matvejbernat <mbernat08@mail.ru>
2026-04-29 13:51:32 +0300

    Lab 5: fix

--- obj/Debug/net10.0/GraphicsLabAvalonia.AssemblyInfo.cs
+++ obj/Debug/net10.0/GraphicsLabAvalonia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GraphicsLabAvalonia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8c1d80dd9562cc6a56b90db1b663454410d8400")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25e220c9fb5f314022220ea955b4df2506acae61")]
 [assembly: System.Reflection.AssemblyProductAttribute("GraphicsLabAvalonia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GraphicsLabAvalonia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
